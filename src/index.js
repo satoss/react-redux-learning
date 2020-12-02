6b2db0f33b2cea3ccc3819ceb4006d55c19a2858
shoei_sato <46258448+satoss@users.noreply.github.com>
2020-12-02 22:45:31 +0900

    implement event new component

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import "./index.css";
 import reducer from "./reducers";
 import EventsIndex from "./components/events_index";
 import EventsNew from "./components/events_new";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
@@ -17,7 +17,7 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <Switch>
-        {/* <Route exact path="/events/new" component={EventsNew} /> */}
+        <Route exact path="/events/new" component={EventsNew} />
         <Route exact path="/" component={EventsIndex} />
       </Switch>
     </BrowserRouter>
@@ -28,4 +28,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
