403dae598a3e047bb2d45a3a75e2c2970de1ceb0
shoei_sato <46258448+satoss@users.noreply.github.com>
2021-01-06 18:25:28 +0900

    apply material-ui

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -3,6 +3,17 @@ import { connect } from "react-redux";
 import { readEvents } from "../actions";
 import _ from "lodash";
 import { Link } from "react-router-dom";
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from "material-ui/Table";
+import FloatingActionButton from "material-ui/FloatingActionButton";
+import ContentAdd from "material-ui/svg-icons/content/add";
+import { DeviceGpsFixed } from "material-ui/svg-icons";
 
 class EventsIndex extends Component {
   componentDidMount() {
@@ -11,30 +22,42 @@ class EventsIndex extends Component {
 
   renderEvents() {
     return _.map(this.props.events, (events) => (
-      <tr key={events.id}>
-        <td>{events.id}</td>
-        <td>
+      <TableRow key={events.id}>
+        <TableRowColumn>{events.id}</TableRowColumn>
+        <TableRowColumn>
           <Link to={`/events/${events.id}`}>{events.title}</Link>
-        </td>
-        <td>{events.body}</td>
-      </tr>
+        </TableRowColumn>
+        <TableRowColumn>{events.body}</TableRowColumn>
+      </TableRow>
     ));
   }
 
   render() {
+    const style = {
+      position: "fixed",
+      right: 12,
+      bottom: 12,
+    };
     return (
       <React.Fragment>
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Title</th>
-              <th>Body</th>
-            </tr>
-          </thead>
-          <tbody>{this.renderEvents()}</tbody>
-        </table>
-        <Link to="/events/new">New Event</Link>
+        <FloatingActionButton
+          style={style}
+          containerElement={<Link to="/events/new" />}
+        >
+          <ContentAdd />
+        </FloatingActionButton>
+        <Table>
+          <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+            <TableRow>
+              <TableHeaderColumn>ID</TableHeaderColumn>
+              <TableHeaderColumn>Title</TableHeaderColumn>
+              <TableHeaderColumn>Body</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody displayRowCheckbox={false}>
+            {this.renderEvents()}
+          </TableBody>
+        </Table>
       </React.Fragment>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
 import "./index.css";
 import reducer from "./reducers";
@@ -21,16 +22,18 @@ const enhancer =
 const store = createStore(reducer, enhancer);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/events/new" component={EventsNew} />
-        <Route path="/events/:id" component={EventsShow} />
-        <Route exact path="/" component={EventsIndex} />
-        <Route exact path="/events" component={EventsIndex} />
-      </Switch>
-    </BrowserRouter>
-  </Provider>,
+  <MuiThemeProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/events/new" component={EventsNew} />
+          <Route path="/events/:id" component={EventsShow} />
+          <Route exact path="/" component={EventsIndex} />
+          <Route exact path="/events" component={EventsIndex} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById("root")
 );
 
